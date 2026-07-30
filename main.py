5f1115cc1f45d502b59f90b3897821b3ff3b21aa
codebyasim <216580305+codebyasim@users.noreply.github.com>
2026-07-30 12:22:13 +0100

    Add load first five machine data

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
+# Load manufacturing machine data
+data = pd.read_csv("data/ai4i2020.csv")
+
+# Display the first five machines
+print(data.head())
+
 print("SmartFactory AI")
 print("Libraries loaded successfully!")
